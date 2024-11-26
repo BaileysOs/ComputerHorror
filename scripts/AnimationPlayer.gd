adeb8606b9f054c1ac15ba4ff6d44cdefcc931e5
BaileysOs <florian.schwarzer@mailfence.com>
2024-11-26 14:20:24 +0100

    Added skip and start

--- scripts/AnimationPlayer.gd
+++ scripts/AnimationPlayer.gd
@@ -1,4 +1,14 @@
 extends AnimationPlayer
 
+@onready var audio_player = $AudioStreamPlayer
+
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
+		seek(current_animation_length, true)
+		audio_player.seek(current_animation_length)
+		
+
+
 func _ready():
 	play("EchoOsBoot")
+	
