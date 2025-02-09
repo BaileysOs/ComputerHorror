06e39fd2dba3ed065de9c051e310b805ee7073bf
BaileysOs <florian.schwarzer@mailfence.com>
2025-02-09 15:33:01 +0100

    text edit added
    sddsf

---
+++ Notes.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+var last_click_time = 0
+const DOUBLE_CLICK_TIME = 300
+var note_window_node
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	note_window_node = get_node("Notes")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if get_rect().has_point(to_local(event.position)):
+			var current_time = Time.get_ticks_msec()
+			if current_time - last_click_time <= DOUBLE_CLICK_TIME:
+				print("Double Click detected!")
+				note_window_node.visible = true
+				
+			last_click_time = current_time
